a9995db72fa74e63c6c71909b93d19b23b69a7f8
garnettgont <garnettgont@gmail.com>
2024-03-24 11:59:01 +0300

    create array for weights

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = [100, 5, 4, 7, 8, 35, 1, 3, 8, 500];
+int i = 0;
+int max = array[0];
+while (i+1 < array.Length)
+{
+    if(array[i+1] > max)
+    {
+        max = array[i+1];
+    }
+    i = i+1;
+}
+Console.Write(max);
