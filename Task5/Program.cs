bb4def9128820a74a6e097adf671d595ffbbd01c
garnettgont <garnettgont@gmail.com>
2024-03-24 12:54:43 +0300

    create array for weights with for and foreach

---
+++ Task5/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = [100, 5, 4, 7, 8, 35, 1, 3, 8, 500];
+int max = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+}
+Console.Write(max);
+//OR
+for (int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+Console.Write(max);
